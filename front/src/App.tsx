990de5f86093a9448c60b48aa1365d55a72c64e5
ota <otavio.cts@gmail.com>
2025-03-11 19:23:24 -0300

    Init project with back and front

---
+++ front/src/App.tsx
@@ -0,0 +1,19 @@
+import { useEffect, useState } from 'react'
+
+function App() {
+  const [hello, setHello] = useState('')
+
+  useEffect(() => {
+    fetch('/').then(async (res) => {
+      const txt = await res.text()
+      console.log(txt)
+      setHello(txt)
+    })
+  })
+
+  return <>
+    <h1>{hello}</h1>
+  </>
+}
+
+export default App
