3d0ec95a53c15e29b6e39d8be51e62400eda10dd
ota <otavio.cts@gmail.com>
2025-03-12 10:35:26 -0300

    Add initial structure

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,19 +1,60 @@
 import { useEffect, useState } from 'react'
 
-function App() {
-  const [hello, setHello] = useState('')
-
-  useEffect(() => {
-    fetch('http://localhost:3000/').then(async (res) => {
-      const txt = await res.text()
-      console.log(txt)
-      setHello(txt)
-    })
-  })
-
-  return <>
-    <h1 className='text-red-500'>{hello}</h1>
-  </>
+type TaskDate = {
+  day: number
+  month: number
+  year: number
 }
 
-export default App
+type Task = {
+  title: string
+  time: number
+}
+
+export default function App() {
+  const today = new Date()
+  const [date, setDate] = useState<TaskDate>({
+    day: today.getDate(),
+    month: today.getMonth() + 1,
+    year: today.getFullYear(),
+  })
+
+  const [tasks, setTasks] = useState<Task[]>(randomTasks(5))
+
+  useEffect(() => {})
+
+  return (
+    <>
+      <div className="flex justify-around text-red-500">
+        <div>&lt;</div>
+        <div>
+          {date.day} / {date.month} / {date.year}
+        </div>
+        <div>&gt;</div>
+      </div>
+
+      <div className="flex flex-col items-center justify-center">
+        {tasks.map(task => (
+          <div className="m-2 flex w-1/2 cursor-pointer justify-center rounded border p-2 hover:bg-amber-100">
+            {task.title}
+          </div>
+        ))}
+      </div>
+    </>
+  )
+}
+
+function randomTasks(n: number): Task[] {
+  const titles = ['Task A', 'Task B', 'Task C', 'Task D', 'Task E']
+
+  const tasks: Task[] = []
+
+  for (let i = 0; i < n; i++) {
+    const title = titles[Math.floor(Math.random() * titles.length)]
+    const time = Math.floor(Math.random() * 100) + 1
+
+    tasks.push({ title, time })
+  }
+
+  return tasks
+}
