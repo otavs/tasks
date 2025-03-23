0a8d392a51eed071586437e13c3939e675b8cf4d
ota <otavio.cts@gmail.com>
2025-03-23 19:18:46 -0300

    Update positions on delete

--- back/src/index.ts
+++ back/src/index.ts
@@ -139,11 +139,31 @@ app.delete('/tasks/:id', async (req: Request, res: Response) => {
   const { id } = req.params
 
   try {
-    const deletedTask = await prisma.task.delete({
+    const taskToDelete = await prisma.task.findUnique({
       where: { id: Number(id) },
     })
-    res.json({ message: 'Task deleted successfully', task: deletedTask })
+
+    if (!taskToDelete) {
+      res.status(404).json({ error: 'Task not found' })
+      return
+    }
+
+    const { position, day, month, year } = taskToDelete
+
+    await prisma.$transaction(async (tx) => {
+      await tx.task.delete({
+        where: { id: Number(id) },
+      })
+
+      await tx.task.updateMany({
+        where: { position: { gt: position }, day, month, year },
+        data: { position: { decrement: 1 } },
+      })
+    })
+
+    res.json({ message: 'Task deleted successfully', task: taskToDelete })
   } catch (error) {
+    console.error('Error deleting task:', error)
     res.status(500).json({ error: 'Failed to delete task' })
   }
 })
