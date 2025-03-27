4ed75aafe026f5f4b097007581721cd83e7cc644
ota <otavio.cts@gmail.com>
2025-03-27 19:53:17 -0300

    .env front

--- front/src/api/tasks.ts
+++ front/src/api/tasks.ts
@@ -3,7 +3,7 @@ import { dateAtom } from '../state.ts'
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import { TaskCreateModel } from '../types.tsx'
 
-const host = 'http://localhost:3000'
+const host = import.meta.env.VITE_HOST
 
 export const useGetTasksQuery = () => {
   const [date] = useAtom(dateAtom)
