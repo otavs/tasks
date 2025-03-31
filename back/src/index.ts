339bf676f09dec4a16cd5b955c71dd26a50ff2d5
ota <otavio.cts@gmail.com>
2025-03-31 01:33:34 -0300

    Serve front from back on prod

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,18 +1,24 @@
 import express from 'express'
 import cors from 'cors'
-
+import path from 'path'
+import { fileURLToPath } from 'url'
 import { taskRouter } from './routers/taskRouter.ts'
 import { rateLimiter } from './middlewares/rateLimit.ts'
 
-const port = 3000
-
 const app = express()
+
+app.use(rateLimiter)
+
+if (process.env.ENV === 'production') {
+  const dirName = path.dirname(fileURLToPath(import.meta.url))
+  const frontDistPath = path.join(dirName, '../../front/dist')
+  app.use(express.static(frontDistPath))
+}
 
 app.use(express.json())
 app.use(cors())
-app.use(rateLimiter)
 app.use('/tasks', taskRouter)
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`)
+app.listen(process.env.PORT, () => {
+  console.log(`Server listening on port ${process.env.PORT}`)
 })
