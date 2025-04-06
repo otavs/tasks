d38b49ce03266f44ea962a6c058897d03457d135
ota <otavio.cts@gmail.com>
2025-04-06 18:50:54 -0300

    Modal form higher up

--- front/src/components/Modal.tsx
+++ front/src/components/Modal.tsx
@@ -34,7 +34,7 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
       <div className="fixed inset-0 bg-gray-500/75 transition-opacity" aria-hidden="true" onClick={onClose}></div>
 
       <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-        <div className="flex min-h-full items-center justify-center p-4 text-center">
+        <div className="mt-[25vh] flex min-h-full items-start justify-center p-4 text-center">
           <div
             ref={modalRef}
             className="max-w-mdsm:w-full relative w-11/12 transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:max-w-lg"
